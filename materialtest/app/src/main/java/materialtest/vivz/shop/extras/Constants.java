96cb9b10332a978c6626bef6ef75d5a044c83a5e
Sendil Kumar <sendil.kumar@citrix.com>
2015-06-02 12:12:31 +0530

    removing the irrelevant files and directories

--- materialtest/app/src/main/java/materialtest/vivz/shop/extras/Constants.java
+++
@@ -1,8 +0,0 @@
-package materialtest.vivz.shop.extras;
-
-/**
- * Created by Windows on 13-02-2015.
- */
-public interface Constants {
-    String NA = "NA";
-}
--- materialtest/app/src/main/java/materialtest/vivz/shop/pojo/Information.java
+++
@@ -1,9 +0,0 @@
-package materialtest.vivz.shop.pojo;
-
-/**
- * Created by Windows on 22-12-2014.
- */
-public class Information {
-    public int iconId;
-    public String title;
-}
